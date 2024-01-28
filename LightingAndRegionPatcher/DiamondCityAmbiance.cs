3065f5343b1863b9c564f00f8750e9ee76697470
Ghastly13 <liam.keogh13@gmail.com>
2024-01-28 00:33:02 -0500

    No longer just an error fix, also added support for Diamond City Ambiance.

---
+++ LightingAndRegionPatcher/DiamondCityAmbiance.cs
@@ -0,0 +1,14 @@
+using Mutagen.Bethesda.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightingAndRegionPatcher
+{
+    public static class DiamondCityAmbiance
+    {
+        public static readonly ModKey ModKey = ModKey.FromNameAndExtension("DiamondCityAmbiance.esp");
+    }
+}
